bc4e0f0fc4feffd4202e765ebfb2c7caa3f6018f
Javiersansano1989 <javier.lopezllumnos.usm.cl>
2020-12-17 23:07:47 -0300

    xml

--- data/download-script.R
+++ data/download-script.R
@@ -1,2 +1,9 @@
 fileUrl<-"https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
 download.file("https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD", "cameras.csv", method = "curl")
+
+xmlfileURL <- "https://www.w3schools.com/xml/simple.xml"
+download.file(xmlfileURL, destfile = "./data/simple.xml")
+doc <- xmlTreeParse("./data/simple.xml", useInternalNodes = TRUE)
+
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
